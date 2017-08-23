3838e97f4c585896c483bb838943dbfab3d2eefb
proxi <proxi@gazeta.pl>
2017-08-23 22:05:39 +0200

    Refactor.

--- Arrays/DynamicArray/Main.hs
+++ Arrays/DynamicArray/Main.hs
@@ -2,16 +2,16 @@ import Control.Monad
 import Data.Array.IO
 import Data.Bits
 import Data.IORef
-import Data.Sequence
+import Data.Sequence hiding (replicate)
 
 ints :: String -> [Int]
 ints = map (read :: String -> Int) . words
 
 main :: IO ()
 main = do
-    [n, q] <- fmap (map (read :: String -> Int) . words) getLine :: IO [Int]
-    qs <- fmap (map ints . lines) getContents :: IO [[Int]]
-    seqList <- newListArray (0, n - 1) $ Prelude.replicate n empty :: IO (IOArray Int (Seq Int))
+    [n, q] <- ints <$> getLine :: IO [Int]
+    qs <- (map ints . lines) <$> getContents :: IO [[Int]]
+    seqList <- newListArray (0, n - 1) $ replicate n empty :: IO (IOArray Int (Seq Int))
     lastAnswer <- newIORef 0
     forM_ qs $ \[t, x, y] -> do
         la <- readIORef lastAnswer
