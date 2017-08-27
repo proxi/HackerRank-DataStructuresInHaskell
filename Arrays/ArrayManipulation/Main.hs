ec7263c185afad965188cfd3d90c081e55a165ca
proxi <proxi@gazeta.pl>
2017-08-27 21:12:24 +0200

    Refactoring.

--- Arrays/ArrayManipulation/Main.hs
+++ Arrays/ArrayManipulation/Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE BangPatterns #-}
-
 import Control.Monad
 import Data.Array.Unboxed
 import Data.Array.ST
@@ -8,6 +6,7 @@ import qualified Data.ByteString.Lazy.Char8 as BS
 import Prelude hiding (words)
 
 addArray :: (Num a1, Ix i, MArray a a1 m) => a i a1 -> i -> a1 -> m ()
+{-# INLINE addArray #-}
 addArray arr i e = do
     x <- readArray arr i
     writeArray arr i (x + e)
@@ -28,13 +27,8 @@ main = do
             arr <- newArray (1, n) 0
             forM_ qs (\[a, b, k] -> do
                 addArray arr a k
-                when ((b + 1) <= n) $ addArray arr (b + 1) (-k)
-                return ())
-            forM_ [2..n] (\i -> do
-                e <- readArray arr (i - 1)
-                f <- readArray arr i
-                writeArray arr i (e+f)
-                return ())
+                when ((b + 1) <= n) $ addArray arr (b + 1) (-k))
+            forM_ [2..n] (\i -> readArray arr (i - 1) >>= addArray arr i)
             return arr
     print $ maximum $ map (\i -> arrx ! i) [1..n]
     return ()
